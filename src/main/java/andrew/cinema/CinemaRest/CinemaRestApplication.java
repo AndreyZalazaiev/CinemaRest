7ed5d76032460ad7e7773be1056fba25c4756477
Bergmah <52858177+Bergmah@users.noreply.github.com>
2020-03-21 09:53:04 +0200

    Initial commit: dsfs

---
+++ src/main/java/andrew/cinema/CinemaRest/CinemaRestApplication.java
@@ -0,0 +1,13 @@
+package andrew.cinema.CinemaRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andrew/cinema/CinemaRest/CinemaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package andrew.cinema.CinemaRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
