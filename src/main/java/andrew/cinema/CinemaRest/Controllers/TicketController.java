499b32b53830eb181fbe2cf18d2d258b4f692fa5
Bergmah <52858177+Bergmah@users.noreply.github.com>
2020-04-24 20:15:31 +0300

    Initialize commit: emails

--- src/main/java/andrew/cinema/CinemaRest/Controllers/TicketController.java
+++ src/main/java/andrew/cinema/CinemaRest/Controllers/TicketController.java
@@ -1,15 +1,23 @@
 package andrew.cinema.CinemaRest.Controllers;
 
-import andrew.cinema.CinemaRest.Entities.*;
-import andrew.cinema.CinemaRest.Repositories.*;
+import andrew.cinema.CinemaRest.Entities.account;
+import andrew.cinema.CinemaRest.Entities.session;
+import andrew.cinema.CinemaRest.Entities.ticket;
+import andrew.cinema.CinemaRest.Repositories.accountRepos;
+import andrew.cinema.CinemaRest.Repositories.sessionRepos;
+import andrew.cinema.CinemaRest.Repositories.ticketRepos;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.criteria.CriteriaBuilder;
+import javax.swing.text.html.HTML;
 
 
 @RestController
 public class TicketController {
+    @Autowired
+    private JavaMailSender javaMailSender;
     @Autowired
     private accountRepos accRep;
     @Autowired
@@ -49,6 +57,9 @@ public class TicketController {
         String [] places = place.split(",");
         String [] rows = row.split(",");
         String out="";
+        StringBuilder mailText = new StringBuilder();
+        mailText.append("Purchase information from best cinema in the world\n");
+        String to="";
         for(int i=0;i<prices.length;i++) {
             ticket tk = new ticket();
             tk.setIdsession(idsession);
@@ -57,13 +68,18 @@ public class TicketController {
             tk.setPlace(Integer.parseInt(places[i]));
             tk.setRownum(Integer.parseInt(rows[i]));
             account ac = accRep.findByIdaccount(idaccount);
+            to=ac.getEmail();
             session sn = sessionRep.findByIdsession(idsession);
             out+="place:"+places[i]+",row: "+rows[i]+"   ;";
+            mailText.append("#"+(i+1)+"\t");
+            mailText.append("Place: "+places[i] +"  and Row: "+rows[i]+"\n");
             ac.setTk(tk);
             sn.setTk(tk);
             sessionRep.save(sn);
             accRep.save(ac);
         }
+        mailText.append("Have a nice day!\nDo not forget to flex");
+        sendEmail(to,"Сinema Flex",mailText.toString());
         return "Saved: "+out;
     }
     @RequestMapping("/tickets/this")
@@ -81,5 +97,16 @@ public class TicketController {
     String Remove(@RequestParam int id) {
         ticketRep.deleteById(id);
         return "deleted id:" + id;
+    }
+    void sendEmail(String to,String subject,String text) {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(to);
+
+        msg.setSubject(subject);
+        msg.setText(text);
+
+        javaMailSender.send(msg);
+
     }
 }
